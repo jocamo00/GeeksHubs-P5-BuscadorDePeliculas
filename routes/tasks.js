d1ecc8cbb14a5a136a82dee5758e4e31b5ed0999
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-30 01:14:28 +0200

    Endpoint crear una tarea

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,5 +16,16 @@ router.get('/', (req, res) => {
 });
 
 
+//Crear una tarea
+router.post('/', (req, res) => {
+    Task.create({...req.body})
+        .then(tasks => res.status(201).send(tasks))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al crear la tarea'
+            });
+        });
+});
 
 module.exports = router;
