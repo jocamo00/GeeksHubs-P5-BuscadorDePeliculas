a90f1347521ddedf38c4a002a9c4ec5ac11db2be
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-30 01:15:43 +0200

    Endpoint mostrar todos los usuarios

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,17 @@ const { User } = require('../models/index.js');
 
 
 
+//Mostrar todos los usuario
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(users => res.send(users))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar los usuarios'
+            });
+        });
+});
 
 
 
