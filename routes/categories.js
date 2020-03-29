2f1ce1901f7029b3f5261a4dd949c8da6829856e
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-30 01:12:12 +0200

    Endpoint crear una categoria

--- routes/categories.js
+++ routes/categories.js
@@ -15,5 +15,16 @@ router.get('/', (req, res) => {
 });
 
 
+//Crear una categoria
+router.post('/', (req, res) => {
+    Category.create({...req.body})
+        .then(categories => res.status(201).send(categories))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al crear la categoria'
+            });
+        });
+});
 
 module.exports = router;
