ec40a5084eec47c9112b0501edf927f531d29b66
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-29 21:53:20 +0200

    Asociaciones de category con Task

--- models/category.js
+++ models/category.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Category.associate = function(models) {
     // associations can be defined here
-    
+    //Una categoria tiene muchas tareas
+    Category.hasMany(models.Task);
   };
   return Category;
 };
