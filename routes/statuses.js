19465010a062826ef2c54c1d10de835a01477516
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-29 21:59:36 +0200

    Endpoint mostrar todos los estados

--- routes/statuses.js
+++ routes/statuses.js
@@ -2,6 +2,17 @@ const router = require('express').Router();
 const { Status } = require('../models/index.js');
 
 
+//Mostrar todos los estados
+router.get('/', (req, res) => {
+    Status.findAll()
+        .then(statuses => res.send(statuses))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar los estados'
+            });
+        })
+})
 
 
 
