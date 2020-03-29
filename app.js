cf6c50edde3708649f202225ba69e560e52ecc5b
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-29 21:40:35 +0200

    Ruta statuses creada, configurada y importada a app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tasksRouter = require('./routes/tasks');
 var categoriesRouter = require('./routes/categories');
+var statusesRouter = require('./routes/statuses');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
 app.use('/categories', categoriesRouter);
+app.use('/statuses', statusesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/statuses.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Status } = require('../models/index.js');
+
+
+
+
+
+
+module.exports = router;
