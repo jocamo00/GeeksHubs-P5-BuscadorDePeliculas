a223a1c59d65ccf40de9f6d4e6ada097b0ff283f
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-03-30 01:13:28 +0200

    Endpoint mostrar todas las tareas

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,17 @@ const { Task } = require('../models/index.js');
 
 
 
+//Mostrar todas las tareas
+router.get('/', (req, res) => {
+    Task.findAll()
+        .then(tasks => res.send(tasks))
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar las tareas'
+            });
+        });
+});
 
 
 
