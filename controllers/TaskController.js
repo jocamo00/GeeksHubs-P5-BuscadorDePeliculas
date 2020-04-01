67d1bc3341bdb4d76e9ef4e046e3a3a10903a11b
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-04-02 01:17:07 +0200

    Filtrado de task por id

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -12,6 +12,22 @@ const TaskController = {
                 });
             });
     },
+
+    getId(req, res) {
+        let _id = req.params.id
+
+        Task.findAll({
+            where: { id: _id }
+        })
+        .then(tasks => { res.send(tasks)})
+        .catch(err => {
+            console.log(err)
+            res.status(500).send({
+                message: 'Ha habido un problema al cargar la tarea'
+            });
+        });
+    },
+
     insert(req, res) {
         Task.create({...req.body})
             .then(tasks => res.status(201).send(tasks))
@@ -22,6 +38,7 @@ const TaskController = {
                 });
             });
     }
+    
 }
 
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,9 @@ const TaskController = require('../controllers/TaskController.js');
 //Mostrar todas las tareas
 router.get('/', TaskController.getAll);
 
+//Mostrar una tarea por id
+router.get('/id/:id', TaskController.getId);
+
 //Crear una tarea
 router.post('/', TaskController.insert );
 
