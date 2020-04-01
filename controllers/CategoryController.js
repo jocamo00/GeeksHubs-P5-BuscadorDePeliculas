e2cb0c9eff86d0de99d9adc4d10fe1bea002af7c
Jose Carreres <alan.brito.delgado.1972@gmail.comjose.carreres.81@gmail.com>
2020-04-02 01:20:50 +0200

    Filtrado de Category por id

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -12,6 +12,22 @@ const CategoryController = {
                 });
             });
     },
+
+    getId(req, res) {
+        let _id = req.params.id
+
+        Category.findAll({
+                where: { id: _id }
+            })
+            .then(categories => { res.send(categories)})
+            .catch(err => {
+                console.log(err)
+                res.status(500).send({
+                    message: 'Ha habido un problema al cargar la categoria'
+                });
+            });
+    },
+
     insert(req, res) {
         Category.create({...req.body})
             .then(categories => res.status(201).send(categories))
--- routes/categories.js
+++ routes/categories.js
@@ -5,6 +5,9 @@ const CategoryController = require('../controllers/CategoryController.js');
 //Mostrar todas las categorias
 router.get('/', CategoryController.getAll);
 
+//Mostrar una categoria por id
+router.get('/id/:id', CategoryController.getId);
+
 //Crear una categoria
 router.post('/', CategoryController.insert);
 
